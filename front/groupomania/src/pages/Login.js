357026394d87e33f5c9ba9019fc92955a588849a
= <julesaintho@gmail.com>
2022-08-12 01:38:09 +0200

    Page auth/login

--- front/groupomania/src/pages/Login.js
+++ front/groupomania/src/pages/Login.js
@@ -1,15 +1,61 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import '../styles/pages/signup.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+
 function Login() {
     return (
-        <div>
-            <h1>LOGIN</h1>
-            <h2>CONEXXIO :</h2>
-            <h3>
-                PAS DE COMPTE ?
-                <NavLink exact to="/signup" activeClassName="navActive">S'inscrire ?</NavLink>
-            </h3>
+        <div className='parent'>
+
+        <div className="left">
+
+            <div className="titles">
+
+            <span>  <FontAwesomeIcon icon={faGlobe} /></span>
+
+                <h1>Connexion</h1>
+                <h2>Coquin va</h2>
+                <h3>Connexion via email</h3>
+
+            </div>
+            
+            <div className="form">
+
+            
+                <div className="form_text">
+                        <h2>Email</h2>
+                        <input type="text" defaultValue="Email"/>
+                </div>
+
+                <div className="form_text">
+                        <h2>Mot de passe</h2>
+                        <input type="text" defaultValue="Mot de passe"/>
+                </div>
+
+                
+                <button>Connexion</button>
+                <h4>
+                Pas encore enregistré ?
+                    <span>
+                        <NavLink exact to="/signup" activeClassName="navActive"> Créer un compte</NavLink>
+                    </span>
+                </h4>
+                <h5>2022@ Groupomania Tout Droits Réservés</h5>
+
+            </div>
+
         </div>
+
+
+        <div className="right">
+            <div className="template">
+                <img src={require("../media/Signup.jpeg")} alt="Groupomania-icon"  className='title_icon'/>   
+            </div>            
+        </div>
+
+
+</div>
     )
 }
 
--- front/groupomania/src/pages/Signup.js
+++ front/groupomania/src/pages/Signup.js
@@ -1,13 +1,71 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 import '../styles/pages/signup.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserTie } from '@fortawesome/free-solid-svg-icons'
+
 function Signup() {
     return (
-        <div>
-        <h1>Bienvenue à Groupomania !</h1>
-        <h1>Inscription</h1>
-        <h2>renseigne tes infos fdp</h2>
-        <NavLink exact to="/login" activeClassName="navActive">Déjà un compte ?</NavLink>
+    <div className='parent'>
+
+            <div className="left">
+
+                <div className="titles">
+                    <h1 className='insc_title'>Inscription   <FontAwesomeIcon icon={faUserTie}/></h1>
+                    <h2>Bienvenue sur le site de Groupomania !</h2>
+                    <h3>Inscription avec Email</h3>
+
+                </div>
+                
+                <div className="form">
+
+                    <div className="form_text">
+                        <h2>Nom
+                        <span>*</span>
+                        </h2>
+                        <input type="text" defaultValue="Nom"/>
+                    </div>
+
+                    <div className="form_text">
+                        <h2>Prénom
+                        <span>*</span>
+                        </h2>
+                        <input type="text" defaultValue="Prénom"/>
+                    </div>
+
+                    <div className="form_text">
+                        <h2>
+                            Email
+                            <span>*</span>
+                        </h2>
+                        <input type="text"defaultValue="Adresse Mail"/>
+                    </div>
+
+                    <div className="form_text">
+                        <h2>Mot de passe
+                        <span>*</span>
+                        </h2>
+                        <input type="text" defaultValue="Mot de passe"/>
+                    </div>
+
+                    <button>Inscription</button>
+                    <h4>
+                       Déjà un compte ?            
+                        <NavLink exact to="/login" activeClassName="navActive"> Connecte-toi</NavLink>
+                    </h4>
+                </div>
+                <h5>2022@ Groupomania Tout Droits Réservés</h5>
+
+            </div>
+
+
+        <div className="right">
+            <div className="template">
+                <img src={require("../media/Signup.jpeg")} alt="Groupomania-icon"  className='title_icon'/>   
+            </div>            
+        </div>
+   
+    
     </div>
     )
 }
