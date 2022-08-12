421ed6bf033049794817389e2bf086564a7db58e
= <julesaintho@gmail.com>
2022-08-13 01:45:16 +0200

    home responsive fix

--- front/groupomania/src/pages/Home.js
+++ front/groupomania/src/pages/Home.js
@@ -7,14 +7,17 @@ import { NavLink } from "react-router-dom";
 function Home() {
     return (
         <div className='home_div'>
+            
             <div className="image">
                   <img src={require("../media/template3.png")} alt="Groupomania-template"  className='home_template'/>   
             </div>
+
             <div className="content">
-            <img src={require("../media/red.png")} alt="Groupomania-template"  className='groupomania_logo'/>   
+                <img src={require("../media/blackIcon.png")} alt="Groupomania-template"  className='groupomania_logo'/>   
                 <h1>Bienvenue sur le site de Groupomania !</h1>
-                <button><NavLink exact to="/login" style={{ textDecoration: 'none', color: "white" }}  > Se Connecter <FontAwesomeIcon icon={faGlobe} /></NavLink></button>
-                <h2>Où alors S'inscrire ?</h2>
+                <h2>Échangez avec vos </h2>
+                <NavLink exact to="/login" style={{ textDecoration: 'none', color: "white" }}  > <button>Se Connecter <FontAwesomeIcon icon={faGlobe} /> </button></NavLink>
+                <h3>Où alors S'inscrire ?</h3>
             </div>
       
         </div>
